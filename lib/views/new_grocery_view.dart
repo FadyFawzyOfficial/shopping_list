985fe3bfd4f2647d53a5cdb7e7a28ddc4c71b8a7
Fady Fawzy <FadyFawzyOfficial@gmail.com>
2024-09-24 11:33:14 +0300

    2. Build form contains TextFormFiled & DropdownButtonFormField

--- lib/views/new_grocery_view.dart
+++ lib/views/new_grocery_view.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import '../data/categories.dart';
+
 class NewGroceryView extends StatefulWidget {
   const NewGroceryView({super.key});
 
@@ -12,9 +14,51 @@ class _NewGroceryViewState extends State<NewGroceryView> {
   Widget build(context) {
     return Scaffold(
       appBar: AppBar(title: const Text('Add new grocery item')),
-      body: const Padding(
-        padding: EdgeInsets.all(16),
-        child: Text('Form'),
+      body: Padding(
+        padding: const EdgeInsets.all(16),
+        child: Form(
+          child: Column(
+            children: [
+              TextFormField(
+                maxLength: 50,
+                decoration: const InputDecoration(labelText: 'Name'),
+              ),
+              Row(
+                crossAxisAlignment: CrossAxisAlignment.end,
+                children: [
+                  Expanded(
+                    child: TextFormField(
+                      initialValue: '1',
+                      decoration: const InputDecoration(labelText: 'Quantity'),
+                    ),
+                  ),
+                  const SizedBox(width: 16),
+                  Expanded(
+                    child: DropdownButtonFormField(
+                      items: [
+                        for (final category in categories.entries)
+                          DropdownMenuItem(
+                            value: category,
+                            child: Row(
+                              children: [
+                                Icon(
+                                  Icons.square_rounded,
+                                  color: category.value.color,
+                                ),
+                                const SizedBox(width: 8),
+                                Text(category.value.name),
+                              ],
+                            ),
+                          ),
+                      ],
+                      onChanged: (value) {},
+                    ),
+                  ),
+                ],
+              )
+            ],
+          ),
+        ),
       ),
     );
   }
